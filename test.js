357d1a2054f9764de39e2ee0a950f2379841f261
LEEKIWAN <guy2895@gmail.com>
2021-06-04 11:37:39 +0900

    짝수 찾기

---
+++ test.js
@@ -0,0 +1,13 @@
+function num(){
+    let Arr=[];
+    for(i=0; i<50; i++){
+        if((i%2)===0){
+            Arr.push(i)
+        }else if ((i%2)!==0){
+            continue
+        }
+    }
+    return Arrgit 
+}
+num()
+
